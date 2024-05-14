71a52763aec7dd69e0b9dfde8c660a3902fafda2
pingu01 <gabrielmnascimento04@gmail.com>
2024-05-14 10:51:49 -0300

    feat? test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div>
       <h1>Inteli - Instituto de Tecnologia e Liderança</h1>
       <p>Já se fazem faculdades como futuramente.</p>
+      <p>Seja bem-vindo ao Inteli!</p>
     </div>
   );
 }
